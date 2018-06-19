289d6c9d469d71f23e859041d333c141d993aeda
RamnathGokul <ramnath1109@gmail.com>
2018-06-19 11:07:00 +0530

    browserhelper changes

--- WebAutomation/src/test/java/core_libs/BrowserHelper.java
+++ WebAutomation/src/test/java/core_libs/BrowserHelper.java
@@ -1,11 +1,14 @@
 package core_libs;
 
+import org.openqa.selenium.UnexpectedAlertBehaviour;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
 import java.util.HashMap;
@@ -35,6 +38,8 @@ public class BrowserHelper {
                 System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/src/test/resources/drivers/geckodriver");
 
                 FirefoxProfile fp = new FirefoxProfile();
+
+                //File download settings
                 fp.setPreference("browser.download.manager.showWhenStarting",false);
                 fp.setPreference("browser.download.folderList", 2);
                 fp.setPreference("browser.download.useDownloadDir", true);
@@ -42,8 +47,13 @@ public class BrowserHelper {
                 fp.setPreference("browser.helperApps.neverAsk.saveToDisk","text/plain,application/pdf,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/msword,application/vnd.ms-powerpoint,application/vnd.openxmlformats-officedocument.presentationml.presentation,application/octet-stream,application/msexcel,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,text/csv,text/txt,application/pkcs10,application/x-msdownload,application/csv,text/html,application/force-download,application/pkix-cert");
                 fp.setPreference("browser.startup.homepage", "http://www.google.com");
 
+                //SSL cert handling
+                fp.setAssumeUntrustedCertificateIssuer(false);
+                fp.setAcceptUntrustedCertificates(true);
+
                 FirefoxOptions fo = new FirefoxOptions();
                 fo.setProfile(fp);
+                fo.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT);
 
                 driver = new FirefoxDriver(fo);
                 break;
@@ -57,9 +67,9 @@ public class BrowserHelper {
                 chromePrefs.put("download.prompt_for_download", "false");
                 chromePrefs.put("profile.default_content_setting_values.automatic_downloads", 1);
 
-
                 ChromeOptions co = new ChromeOptions();
                 co.setAcceptInsecureCerts(true);
+                co.setUnhandledPromptBehaviour(UnexpectedAlertBehaviour.ACCEPT);
                 co.addArguments("test-type");
                 co.addArguments("start-maximized");
                 co.setExperimentalOption("prefs", chromePrefs);
